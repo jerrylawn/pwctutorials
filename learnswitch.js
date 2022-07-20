5a7171f1bb07a3af296c8280c50a8b8226371cf2
jeremiah alao <alaojeremiah@gmail.com>
2022-07-20 14:40:32 +0100

    New Update

---
+++ learnswitch.js
@@ -0,0 +1,37 @@
+// const color = 'red';
+
+// switch (color) {
+//     case 'red':
+//         console.log('color is red')
+//         break;
+//     case 'blue':
+//         console.log('color is blue')
+//         break;
+//     case 'green':
+//         console.log('color is green')
+//         break;
+//     case 'red':
+//         console.log('color is red')
+//         break;
+//     default:
+//         console.log('color is not red, green or blue')
+// }
+
+const score = 53
+
+switch (true) {
+    case score >= 80:
+        console.log('Excellent')
+        break;
+    case score >= 70:
+        console.log('Good')
+        break;
+    case score >= 50:
+        console.log('pass')
+        break;
+    case score <= 40:
+        console.log('failed')
+        break;
+    default:
+        console.log('dont take test')
+}
---
+++ main.js
@@ -0,0 +1,192 @@
+// //Data Types
+// let name = "jeremiah" // string
+// let age = 27 // number
+// let z = null // null
+// let y = undefined // undefined
+
+// let isMary = false; // boolean
+// let favcolor = ["white", "red", "blue", "black"] //array
+
+// let x = "hello world"
+// console.log(age)
+// console.log(name)
+// console.log(y)
+// console.log(isMary)
+// console.log(x.toUpperCase())
+// console.log(x.toLowerCase())
+// console.log(x.length)
+
+// // concatenation with + operator
+
+// let m = "I AM "
+// let n = " I GO TO PWC BOOTCAMP "
+// let p = "I LOVE TO LEARN JAVA SCRIPT "
+// console.log(m + name + n + "and" + p)
+
+// // concatenation with
+
+// console.log(`${m} ${name} ${n} and ${p}`)
+
+// console.log('hello world');
+// console.error('this is an error');
+// console.warn('this is a warning');
+
+// let fruits = ['apple', 'banana', 'mango']
+// fruits.push('grape')
+// fruits.push('strawberry') // add to array
+// fruits.pop() // remove from last
+// fruits.shift()
+// fruits.unshift('mango')
+// fruits[4] = 'blueberries'
+// console.log(fruits)
+
+// //
+// const person = {
+//     name: "jeremiah",
+//     Age: 27,
+//     hobbies: ['music', 'movies', 'sports'],
+// }
+
+// console.log(person.Age)
+
+// // for loop
+// for (let i = 12; i <= 19; i++) {
+//     console.log(i)
+// }
+
+// for (let i = 1; i <= 19; i++) {
+//     console.log("Number is " + i)
+// }
+
+// // while loop
+// let i = 0
+// while (i < 20) {
+//     console.log(i)
+//     i++
+// }
+
+// // if statement
+
+// const x = 10
+// if (x == 20) {
+//     console.log('x is = 20')
+
+// } else {
+//     console.log("x is not = 20")
+// }
+// // if example
+
+// score = 61
+// if (score >= 70) {
+//     console.log("Distinction")
+// }
+// else if (score >= 60) {
+//     console.log("Credit")
+// }
+// else if (score >= 50{
+//     console.log("pass")
+// }
+// else if (score >= 30) {
+//     console.log("failed")
+// }
+// else {
+//     console.log("Did not write Test")
+// }
+
+// creating function
+
+// function addNum(a, b) {
+//     return a + b
+// }
+
+
+
+// function subNum(a, b) {
+//     return a - b
+// }
+
+
+// function mulNum(a, b) {
+
+//     return a * b
+// }
+// console.log(subNum(7, 8))
+// console.log(addNum(3, 4))
+// console.log(mulNum(5, 7))
+
+// new way of function
+
+// const addNum = (a, b) => {
+//     return a + b
+// }
+// const mulNum = (a, b) => {
+//     return a * b
+// }
+// const divNum = (a, b) => {
+//     return a / b
+// }
+
+// console.log(addNum(4, 3))
+// console.log(mulNum(3, 5))
+// console.log(divNum(10, 2))
+// for (let i = 0; i <= 20; i += 3) {
+//     console.log("number is " + i)
+// }
+
+// let someWords = "Adamu";
+// let someArray = ["A", "D", "A", "M", "U"]
+// console.log(someArray.reverse())
+// console.log(someWords.split(''));
+
+// const stringToArray = (str) => {
+//         let toArray = str.split("")
+//         let toReversed = toArray.reverse("")
+//         let to
+// }
+
+
+// const palidromChecker = (str) {
+//     let reverse = str.split('').reverse.join("")
+//     if (reverse === str)
+//     return
+// }
+
+
+// let string15 = "15"
+// console.log(parseInt(string15))
+
+// new new new
+
+// let someWord = "Adamu"
+// let someArray = ['a', 'b', 'c', 'd', 'e', 'f']
+// //console.log(someArray.revers())
+
+// const reverseString = (str) => {
+//     return str.split('').reverse().join('')
+// }
+
+// const palindromeChecker = (str) => {
+//     let revers = str.split('').revers('').join('')
+//     if (revers === str) {
+//         return `${str} is a palindrom`
+//     } else {
+//         return `${str} is not a palindrom`
+//     }
+
+// }
+
+
+
+
+
+// const intPalindrome = (num) => {
+//     let toString = num.toString();
+//     let reversed = toString.split('').reverse().join('')
+//     if (toString === reversed) {
+//         return `${toString} is a palindrome`
+//     } else {
+//         return `${toString} is not a palindrome`
+//     }
+
+//     console.log(intPalindrom(141141))
+// end of new new new
